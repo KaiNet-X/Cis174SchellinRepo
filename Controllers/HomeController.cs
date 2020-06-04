d384aa523d299ef42e9b6bc7a32fdb6b3627c14d
Ultimatenerd1 <ultimatenerdx00@gmail.com>
2020-06-03 22:54:50 -0500

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DbWebAppSchellin.Models;
+
+namespace DbWebAppSchellin.Controllers
+{
+    public class HomeController : Controller
+    {
+        ContactContext context;
+        public HomeController(ContactContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Contact> contacts = context.Contacts.OrderBy(c => c.ID).ToList();
+            return View(contacts);
+        }
+    }
+}
---
+++ obj/Release/netcoreapp3.1/PubTmp/EFSQLScripts/DbWebAppSchellin.Models.ContactContext.sql
@@ -0,0 +1,33 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200603173751_m')
+BEGIN
+    CREATE TABLE [Contacts] (
+        [ID] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        [PhoneNumber] nvarchar(max) NOT NULL,
+        [Address] nvarchar(max) NULL,
+        [Note] nvarchar(max) NULL,
+        CONSTRAINT [PK_Contacts] PRIMARY KEY ([ID])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200603173751_m')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20200603173751_m', N'3.1.4');
+END;
+
+GO
+
