56fa9fa52fcc9f2c2dd1a6f1f9b3171d7c87ab74
Ultimatenerd1 <ultimatenerdx00@gmail.com>
2020-07-24 20:08:26 -0500

    Added Tag helpers and view component

---
+++ ProjectCentral/ProjectCentral/Areas/Ticketing/TagHelpers/SubmitButtonTagHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ProjectCentral.Areas.Ticketing.TagHelpers
+{
+    [HtmlTargetElement("button", Attributes ="submit")]
+    public class SubmitButtonTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            object style = output.Attributes.ContainsName("style") ? output.Attributes["style"].Value : null;
+            output.Attributes.SetAttribute("style", style?.ToString() + ";background-image: linear-gradient(to right, red , yellow);");
+        }
+    }
+}
---
+++ ProjectCentral/ProjectCentral/Areas/Ticketing/ViewComponents/StatusViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectCentral.Areas.Ticketing.Models;
+
+namespace ProjectCentral.Areas.Ticketing.ViewComponents
+{
+    public class StatusViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(Ticket ticket)
+        {
+            return View(ticket);
+        }
+    }
+}
